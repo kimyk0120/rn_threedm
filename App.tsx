98486527cf532153d65e77660106d0b4e088a5d3
kimyk0120 <kimyk0120@hotmail.com>
2023-05-22 14:04:49 +0900

    mod App.tsx - today 버튼 구현

--- App.tsx
+++ App.tsx
@@ -1,28 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useRef, useState} from 'react';
 import {Alert, StyleSheet, Text, View, TouchableOpacity, SafeAreaView} from 'react-native';
-import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
+import {Agenda, DateData, AgendaEntry, AgendaSchedule, CalendarProvider} from 'react-native-calendars';
 // import testIDs from '../testIDs';
 
 interface State {
     items?: AgendaSchedule;
 }
 
-export default class AgendaScreen extends Component<State> {
-    state: State = {
-        items: undefined
-    };
+// interface state{
+//     items: undefined
+// }
+
+export default function AgendaScreen () {
+
+    const [selectedDate, setSelectedDate] = useState(()=>{
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = ('0' + (today.getMonth() + 1)).slice(-2);
+        const day = ('0' + today.getDate()).slice(-2);
+        return year + '-' + month + '-' + day;
+    });
+
+    const [state, setState] = useState<State>({});
 
     // reservationsKeyExtractor = (item, index) => {
     //   return `${item?.reservation?.day}${index}`;
     // };
 
-    loadItems = (day: DateData) => {
-        const items = this.state.items || {};
+    const loadItems = (day: DateData) => {
+        const items = state?.items || {};
 
         setTimeout(() => {
             for (let i = -15; i < 85; i++) {
                 const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-                const strTime = this.timeToString(time);
+                const strTime = timeToString(time);
 
                 if (!items[strTime]) {
                     items[strTime] = [];
@@ -42,13 +53,14 @@ export default class AgendaScreen extends Component<State> {
             Object.keys(items).forEach(key => {
                 newItems[key] = items[key];
             });
-            this.setState({
+
+            setState({
                 items: newItems
             });
         }, 1000);
     }
 
-    renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
         const fontSize = isFirst ? 16 : 14;
         const color = isFirst ? 'black' : '#43515c';
 
@@ -63,7 +75,7 @@ export default class AgendaScreen extends Component<State> {
         );
     }
 
-    renderEmptyDate = () => {
+    const renderEmptyDate = () => {
         return (
             <View style={styles.emptyDate}>
                 <Text>This is empty date!</Text>
@@ -71,47 +83,171 @@ export default class AgendaScreen extends Component<State> {
         );
     }
 
-    rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+    const rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
         return r1.name !== r2.name;
     }
 
-    timeToString(time: number) {
+    const timeToString = (time: number) => {
         const date = new Date(time);
         return date.toISOString().split('T')[0];
     }
+    const goToToday = () => {
+        // Perform any action to navigate to today's date
+        console.log('Go to Today');
 
-    render() {
-        return (
-            <SafeAreaView style={styles.container}>
-                <Agenda
-                    // testID={testIDs.agenda.CONTAINER}
-                    items={this.state.items}
-                    loadItemsForMonth={this.loadItems}
-                    selected={'2017-05-16'}
-                    renderItem={this.renderItem}
-                    renderEmptyDate={this.renderEmptyDate}
-                    rowHasChanged={this.rowHasChanged}
-                    showClosingKnob={true}
-                    // markingType={'period'}
-                    // markedDates={{
-                    //    '2017-05-08': {textColor: '#43515c'},
-                    //    '2017-05-09': {textColor: '#43515c'},
-                    //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-                    //    '2017-05-21': {startingDay: true, color: 'blue'},
-                    //    '2017-05-22': {endingDay: true, color: 'gray'},
-                    //    '2017-05-24': {startingDay: true, color: 'gray'},
-                    //    '2017-05-25': {color: 'gray'},
-                    //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-                    // monthFormat={'yyyy'}
-                    // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-                    //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-                    // hideExtraDays={false}
-                    // showOnlySelectedDayItems
-                    // reservationsKeyExtractor={this.reservationsKeyExtractor}
-                />
-            </SafeAreaView>
-        ); //.render > .return
-    }// .render
+        console.log(selectedDate);
+
+        setSelectedDate(new Date().toISOString().slice(0, 10));
+    };
+
+    const onDayPress = (day:DateData) => {
+        console.log('day pressed', day.dateString);
+        setSelectedDate(day.dateString);
+    }
+
+    const tests:any =
+        {'2012-05-22': [{name: 'item 1 - any js object'}],
+        '2012-05-23': [{name: 'item 2 - any js object', height: 80}],
+        '2012-05-24': [],
+        '2012-05-25': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
+        }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <CalendarProvider date={selectedDate}>
+            <Agenda
+                selected={selectedDate}
+                testID={'kimyk'}
+                // items={state?.items}
+                items={tests}
+                // loadItemsForMonth={loadItems}
+                loadItemsForMonth={month => {
+                    console.log('trigger items loading');
+                }}
+                onCalendarToggled={calendarOpened => {
+                    console.log('onCalendarToggled :' , calendarOpened);
+                }}
+                onDayPress={onDayPress}
+                onDayChange={day => {
+                    console.log('day changed');
+                }}
+                // selected={selectedDate}
+
+                // Specify how each item should be rendered in agenda
+                renderItem={(item, firstItemInDay) => {
+                    return (
+                        <View>
+                            <Text> renderItem test</Text>
+                        </View>
+                    );
+                }}
+
+                // Specify how each date should be rendered. day can be undefined if the item is not first in that day
+                renderDay={(day, item) => {
+                    return (
+                        <View>
+                            <Text> renderDay test</Text>
+                        </View>
+                    );
+                }}
+                // Specify how empty date content with no items should be rendered
+                renderEmptyDate={() => {
+                    return (
+                        <View>
+                            <Text> renderEmptyDate test</Text>
+                        </View>
+                    );
+                }}
+
+                // Specify how agenda knob should look like
+                // renderKnob={() => {
+                //     return <View />;
+                // }}
+                // Override inner list with a custom implemented component
+                // renderList={listProps => {
+                //     return <MyCustomList {...listProps} />;
+                // }}
+                // Specify what should be rendered instead of ActivityIndicator
+                renderEmptyData={() => {
+                    return (
+                        <View>
+                            <Text> renderEmptyData test</Text>
+                        </View>
+                    );
+                }}
+                // Specify your item comparison function for increased performance
+                // rowHasChanged={(r1, r2) => {
+                //     return r1.text !== r2.text;
+                // }}
+                // Hide knob button. Default = false
+                // hideKnob={true}
+                // When `true` and `hideKnob` prop is `false`, the knob will always be visible and the user will be able to drag the knob up and close the calendar. Default = false
+                showClosingKnob={true}
+                // By default, agenda dates are marked if they have at least one item, but you can override this if needed
+                markingType={'multi-dot'}
+                markedDates={{
+                    '2023-05-23': {
+                        marked: true,
+                        dots: [
+                            { key: 'dot1', color: 'red'},
+                            { key: 'dot2', color: 'green' },
+                        ],
+                        selected: false,
+                    },
+                    '2023-05-24': {
+                        marked: true,
+                        dots: [{ key: 'dot3', color: 'blue' }],
+                        dotColor:'black',
+                        selected: false,
+                    },
+                }}
+                // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
+                // disabledByDefault={true}
+                // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly
+                onRefresh={() => console.log('refreshing...')}
+                // Set this true while waiting for new data from a refresh
+                // refreshing={false}
+                // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView
+                // refreshControl={null}
+                // Agenda theme
+                // theme={{
+                //     // ...calendarTheme,
+                //     agendaDayTextColor: 'yellow',
+                //     agendaDayNumColor: 'green',
+                //     agendaTodayColor: 'red',
+                //     agendaKnobColor: 'blue'
+                // }}
+                // Agenda container style
+                // style={{}}
+                // renderItem={renderItem}
+                // renderEmptyDate={renderEmptyDate}
+                // rowHasChanged={rowHasChanged}
+                // showClosingKnob={true}
+                // markingType={'period'}
+                // markedDates={{
+                //    '2017-05-08': {textColor: '#43515c'},
+                //    '2017-05-09': {textColor: '#43515c'},
+                //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+                //    '2017-05-21': {startingDay: true, color: 'blue'},
+                //    '2017-05-22': {endingDay: true, color: 'gray'},
+                //    '2017-05-24': {startingDay: true, color: 'gray'},
+                //    '2017-05-25': {color: 'gray'},
+                //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+                // monthFormat={'yyyy'}
+                // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+                // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+                // hideExtraDays={false}
+                // showOnlySelectedDayItems
+                // reservationsKeyExtractor={this.reservationsKeyExtractor}
+            />
+            {/*  Agenda  */}
+            <TouchableOpacity onPress={goToToday} style={styles.floatingButton}>
+                <Text>Today</Text>
+            </TouchableOpacity>
+
+            </CalendarProvider>
+        </SafeAreaView>
+    ); // .return
 }//.class
 
 const styles = StyleSheet.create({
@@ -130,5 +266,16 @@ const styles = StyleSheet.create({
         height: 15,
         flex: 1,
         paddingTop: 30
-    }
+    },
+    floatingButton: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        backgroundColor: 'blue',
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 2,
+    },
 });
